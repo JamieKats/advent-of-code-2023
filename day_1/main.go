f7b8f4fdc2ddbb4a4f65995b3b0851e761d976d4
JamieKats <jamie.kats@hotmail.com>
2024-02-04 19:25:24 +1000

    Finish day_1 task 2

--- day_1/main.go
+++ day_1/main.go
@@ -36,34 +36,68 @@ func main() {
 
 	var result []string
 
-	for _, line := range split_data {
-		// Scan line for digits
-		for k, v := range wordToDigit {
-			loc := strings.Index(line, k)
-		}
+	var sum int = 0
 
-		// Loop through the line and save first and last value
-		var digitsInLine []string
-		for _, char := range line {
-			// fmt.Println(string(char))
-			if unicode.IsNumber(char) {
-				digitsInLine = append(digitsInLine, string(char))
+	for _, line := range split_data {
+		digitsInLine := []digitPosition{}
+
+		// Scan line for digits
+		for k, _ := range wordToDigit {
+			firstLoc := strings.Index(line, k)
+			lastLoc := strings.LastIndex(line, k)
+
+			if firstLoc != -1 {
+				firstDigit := digitPosition{
+					digit:    wordToDigit[k],
+					position: firstLoc,
+				}
+				digitsInLine = append(digitsInLine, firstDigit)
+			}
+
+			if lastLoc != -1 {
+				lastDigit := digitPosition{
+					digit:    wordToDigit[k],
+					position: lastLoc,
+				}
+				digitsInLine = append(digitsInLine, lastDigit)
 			}
 		}
-		// fmt.Printf("%s%s\n", digitsInLine[0], digitsInLine[len(digitsInLine)-1])
-		var lineResult = digitsInLine[0] + digitsInLine[len(digitsInLine)-1]
-		result = append(result, lineResult)
+
+		// Loop through the line and save first and last numeric
+		for i, char := range line {
+
+			// If ascii digit found create digit position struct and add ti digitsInLine
+			if unicode.IsNumber(char) {
+				digit, err := strconv.Atoi(string(char))
+				if err != nil {
+					panic(err)
+				}
+
+				d := digitPosition{
+					digit:    digit,
+					position: i,
+				}
+
+				digitsInLine = append(digitsInLine, d)
+			}
+		}
+		fmt.Println(digitsInLine)
+
+		fmt.Println(getFirstPlusLastDigitInLine(digitsInLine))
+		lineResult := getFirstPlusLastDigitInLine(digitsInLine)
+
+		sum = sum + lineResult
 	}
 
 	// Sum up all values
-	var sum int = 0
-	for _, val := range result {
-		convertedVal, err := strconv.Atoi(val)
-		if err != nil {
-			panic(err)
-		}
-		sum = sum + convertedVal
-	}
+	// var sum int = 0
+	// for _, val := range result {
+	// 	convertedVal, err := strconv.Atoi(val)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	sum = sum + convertedVal
+	// }
 	fmt.Printf("Sum: %d\n", sum)
 
 	// Write output to file
@@ -76,4 +110,26 @@ func main() {
 	for _, value := range result {
 		fmt.Fprintln(f, value)
 	}
+}
+
+func getFirstPlusLastDigitInLine(digitsInLine []digitPosition) int {
+	firstDigit := digitsInLine[0]
+	lastDigit := digitsInLine[0]
+	for _, digitInLine := range digitsInLine {
+		if digitInLine.position < firstDigit.position {
+			firstDigit = digitInLine
+		}
+
+		if digitInLine.position > lastDigit.position {
+			lastDigit = digitInLine
+		}
+	}
+
+	stringDigit := strconv.Itoa(firstDigit.digit) + strconv.Itoa(lastDigit.digit)
+
+	result, err := strconv.Atoi(stringDigit)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
